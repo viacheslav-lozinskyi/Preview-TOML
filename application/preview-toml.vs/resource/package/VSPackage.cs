a53464f95db38790e3e4e775f37f7cd024ba6fc7
Viacheslav Lozinskyi <vl.spirit@gmail.com>
2020-10-12 22:03:00 +0300

    - MetaPreview is updated to version 1.1.1 - Minor improvements

--- application/preview-toml.vs/resource/package/VSPackage.cs
+++ application/preview-toml.vs/resource/package/VSPackage.cs
@@ -11,11 +11,7 @@ namespace resource.package
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration(CONSTANT.NAME, CONSTANT.DESCRIPTION, CONSTANT.VERSION)]
     [Guid(CONSTANT.GUID)]
-    [ProvideAutoLoad(VSConstants.UICONTEXT.FirstLaunchSetup_string, PackageAutoLoadFlags.BackgroundLoad)]
     [ProvideAutoLoad(VSConstants.UICONTEXT.ShellInitialized_string, PackageAutoLoadFlags.BackgroundLoad)]
-    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExists_string, PackageAutoLoadFlags.BackgroundLoad)]
-    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionHasMultipleProjects_string, PackageAutoLoadFlags.BackgroundLoad)]
-    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionHasSingleProject_string, PackageAutoLoadFlags.BackgroundLoad)]
     public sealed class PreviewTOMLPackage : AsyncPackage
     {
         internal static class CONSTANT
@@ -25,15 +21,17 @@ namespace resource.package
             public const string EXTENSION = ".TOML";
             public const string GUID = "29221199-ABD2-49D4-BA92-3853E3672A65";
             public const string NAME = "Preview-TOML";
-            public const string VERSION = "1.0.4";
+            public const string VERSION = "1.0.5";
         }
 
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
-            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
             {
                 cartridge.AnyPreview.Connect();
                 cartridge.AnyPreview.Register(cartridge.AnyPreview.MODE.PREVIEW, CONSTANT.EXTENSION, new preview.TOML());
+            }
+            {
+                await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
             }
         }
 
